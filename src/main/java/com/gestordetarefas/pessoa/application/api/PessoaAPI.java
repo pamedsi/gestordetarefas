60d514ebc5f31a8461b2c19a8d327d0ee44b3966
Patrick Melo <patrickabimael@yahoo.com.br>
2024-06-12 13:41:07 -0300

    cria camada de controller

--- src/main/java/com/gestordetarefas/pessoa/application/api/PessoaAPI.java
+++ src/main/java/com/gestordetarefas/pessoa/application/api/PessoaAPI.java
@@ -1,11 +1,11 @@
-package com.gestordetarefas.colaborador.application.api;
+package com.gestordetarefas.pessoa.application.api;
 
 import jakarta.validation.*;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api/pessoas")
-public interface ColaboradorAPI {
+public interface PessoaAPI {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     NovaPessoaResponse postNovaPessoa(@RequestBody @Valid NovaPessoaRequest colaboradorDTO);
---
+++ src/main/java/com/gestordetarefas/pessoa/application/api/PessoaRestController.java
@@ -0,0 +1,20 @@
+package com.gestordetarefas.pessoa.application.api;
+
+import com.gestordetarefas.pessoa.application.service.*;
+import lombok.*;
+import lombok.extern.log4j.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@Log4j2
+@RequiredArgsConstructor
+public class PessoaRestController implements PessoaAPI {
+    private final PessoaService pessoaService;
+    @Override
+    public NovaPessoaResponse postNovaPessoa(NovaPessoaRequest colaboradorDTO) {
+        log.info("[inicia]  PessoaRestController - postNovoColaborador");
+        NovaPessoaResponse novaPessoaResponse = pessoaService.adicionarNovaPessoa(colaboradorDTO);
+        log.info("[finaliza]  PessoaRestController - postNovoColaborador\n");
+        return novaPessoaResponse;
+    }
+}
