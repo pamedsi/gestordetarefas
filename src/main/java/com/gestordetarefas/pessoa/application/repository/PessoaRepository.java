28f1ad8581a327e88d019b62bcd22f67631cd1d3
Patrick Melo <patrickabimael@yahoo.com.br>
2024-06-12 19:41:56 -0300

    implementa método de buscar pessoa por UUID

--- src/main/java/com/gestordetarefas/pessoa/application/repository/PessoaRepository.java
+++ src/main/java/com/gestordetarefas/pessoa/application/repository/PessoaRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.*;
 @Repository
 public interface PessoaRepository {
     void salvaNovaPessoa(Pessoa pessoa);
+    Pessoa buscaPessoaPorIdentificador(String identificador);
 }
