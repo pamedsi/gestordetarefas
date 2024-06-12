4bbcb3908533d7b31b835d17e8bfe4e391cdf74c
Patrick Melo <patrickabimael@yahoo.com.br>
2024-06-12 19:41:01 -0300

    implementa put pra atualizar pessoa

--- src/main/java/com/gestordetarefas/pessoa/application/api/PessoaAPI.java
+++ src/main/java/com/gestordetarefas/pessoa/application/api/PessoaAPI.java
@@ -1,6 +1,7 @@
 package com.gestordetarefas.pessoa.application.api;
 
 import jakarta.validation.*;
+import org.hibernate.validator.constraints.*;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,5 +9,8 @@ import org.springframework.web.bind.annotation.*;
 public interface PessoaAPI {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    NovaPessoaResponse postNovaPessoa(@RequestBody @Valid NovaPessoaRequest colaboradorDTO);
+    NovaPessoaResponse postNovaPessoa(@RequestBody @Valid PessoaRequest pessoaRequest);
+    @PutMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    void putPessoa(@RequestBody @Valid PessoaRequest pessoaRequest, @PathVariable("id") @UUID(message = "UUID inválido!") String identificador);
 }
