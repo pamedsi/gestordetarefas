6bae8558f896bdd4d400b43b0ddf7425ad058998
Patrick Melo <patrickabimael@yahoo.com.br>
2024-06-12 11:32:44 -0300

    implementa atributos à tabela

--- src/main/java/com/gestordetarefas/colaborador/domain/Colaborador.java
+++ src/main/java/com/gestordetarefas/colaborador/domain/Colaborador.java
@@ -1,11 +1,21 @@
 package com.gestordetarefas.colaborador.domain;
 
+import com.gestordetarefas.tarefa.domain.*;
 import jakarta.persistence.*;
 
+import java.util.*;
+
 @Entity
-@Table
 public class Colaborador {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @Column
+    private UUID identificador;
+    @Column
+    private String nome;
+    @Column
+    private String departamento;
+    @OneToMany(mappedBy = "colaboradorAlocado")
+    private List<Tarefa> tarefas;
 }
