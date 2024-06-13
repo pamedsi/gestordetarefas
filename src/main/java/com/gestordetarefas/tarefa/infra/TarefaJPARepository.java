5aa2bbda97fd0718f4077fb37cef72d392b2b7f9
Patrick Melo <patrickabimael@yahoo.com.br>
2024-06-13 18:14:16 -0300

    implementa camada de repository da alocação de tarefa

--- src/main/java/com/gestordetarefas/tarefa/infra/TarefaJPARepository.java
+++ src/main/java/com/gestordetarefas/tarefa/infra/TarefaJPARepository.java
@@ -3,5 +3,8 @@ package com.gestordetarefas.tarefa.infra;
 import com.gestordetarefas.tarefa.domain.*;
 import org.springframework.data.jpa.repository.*;
 
+import java.util.*;
+
 public interface TarefaJPARepository extends JpaRepository<Tarefa, Long> {
+    Optional<Tarefa> findByIdentificadorAndDeletadaFalse(UUID identificador);
 }
