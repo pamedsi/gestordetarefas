a25e3fd10226b15381204a3ce5e95b5e1158d0e7
Patrick Melo <patrickabimael@yahoo.com.br>
2024-06-16 19:39:22 -0300

    implementa contagem de tarefas por departamento na camada repository

--- src/main/java/com/gestordetarefas/tarefa/infra/TarefaJPARepository.java
+++ src/main/java/com/gestordetarefas/tarefa/infra/TarefaJPARepository.java
@@ -1,5 +1,6 @@
 package com.gestordetarefas.tarefa.infra;
 
+import com.gestordetarefas.departamento.domain.*;
 import com.gestordetarefas.tarefa.domain.*;
 import org.springframework.data.domain.*;
 import org.springframework.data.jpa.repository.*;
@@ -9,4 +10,5 @@ import java.util.*;
 public interface TarefaJPARepository extends JpaRepository<Tarefa, Long> {
     Optional<Tarefa> findByIdentificadorAndDeletadaFalse(UUID identificador);
     Page<Tarefa> findAllByDeletadaFalseAndPessoaAlocadaNullOrderByPrazoAsc(Pageable pageable);
+    int countByDepartamento(Departamento departamento);
 }
